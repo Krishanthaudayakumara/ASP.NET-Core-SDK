44f831f55e0d5155b7bef2ce888a9fd8a7d4618d
krishanthaudayakumara <udayakumarakrishantha@gmail.com>
2025-09-11 09:02:24 +0530

    Transfer TestBindingProgram to traditional class approach

--- tests/Sitecore.AspNetCore.SDK.RenderingEngine.Integration.Tests/Fixtures/Binding/TestBindingProgram.cs
+++ tests/Sitecore.AspNetCore.SDK.RenderingEngine.Integration.Tests/Fixtures/Binding/TestBindingProgram.cs
@@ -1,31 +1,34 @@
+using System;
+using System.Net.Http;
 using Sitecore.AspNetCore.SDK.LayoutService.Client.Extensions;
 using Sitecore.AspNetCore.SDK.RenderingEngine.Extensions;
 using Sitecore.AspNetCore.SDK.TestData;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+namespace Sitecore.AspNetCore.SDK.RenderingEngine.Integration.Tests.Fixtures.Binding;
 
-builder.Services.AddRouting()
-                .AddMvc();
-
-builder.Services.AddSitecoreLayoutService()
-                .AddHttpHandler("mock", _ => new HttpClient { BaseAddress = new Uri("http://layout.service") })
-                .AsDefaultHandler();
-
-builder.Services.AddSitecoreRenderingEngine();
-
-WebApplication app = builder.Build();
-app.UseSitecoreRenderingEngine();
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Pages}/{action=Index}");
-
-app.Run();
-
-/// <summary>
-/// Partial class allowing this TestProgram to be created by a WebApplicationFactory for integration testing.
-/// </summary>
-public partial class TestBindingProgram
+public class TestBindingProgram
 {
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRouting()
+                        .AddMvc();
+
+        builder.Services.AddSitecoreLayoutService()
+                        .AddHttpHandler("mock", _ => new HttpClient { BaseAddress = new Uri("http://layout.service") })
+                        .AsDefaultHandler();
+
+        builder.Services.AddSitecoreRenderingEngine();
+
+        var app = builder.Build();
+        app.UseSitecoreRenderingEngine();
+        app.UseRouting();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Pages}/{action=Index}");
+
+        app.Run();
+    }
 }
